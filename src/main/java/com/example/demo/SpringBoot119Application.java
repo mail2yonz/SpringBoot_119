d84f50a247d5084f8745f94742fc23be35cfb750
mail2yonz <mail2yonz@gmail.com>
2017-11-08 14:28:00 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot119Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot119Application.class, args);
+	}
+}
